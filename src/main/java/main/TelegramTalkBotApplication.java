c87566ca13017c48d0ace2dc2db089d1a6c5f8ac
Daniil <danielmt.dm@gmail.com>
2017-04-17 13:07:26 +0300

    initial

---
+++ src/main/java/main/TelegramTalkBotApplication.java
@@ -0,0 +1,15 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dmatafonov on 17.04.2017.
+ */
+
+@SpringBootApplication
+public class TelegramTalkBotApplication {
+	public static void main (String[] args){
+		SpringApplication.run(TelegramTalkBotApplication.class, args);
+	}
+}
